d94a3615ae29b1c60a33ac398460c114ec9abdd1
donwon <imdonw@gmail.com>
2014-02-20 22:32:34 -0800

    first commit

---
+++ dw_realtor/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DwRealtor::Application.config.secret_key_base = '6b91c0a77dc6b1b044db4db80df2bd4f0c460beacc1b082da414964a57a45c9090beef2d8e800dac6dc5548f522e6c804ba285a3511061c60ab03085f28235a8'
---
+++ dw_realtor/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DwRealtor::Application.config.session_store :cookie_store, key: '_dw_realtor_session'
