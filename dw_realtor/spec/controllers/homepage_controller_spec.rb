fdb9bf7ce8596393d2ce84eb48583625bfe068a6
donwon <imdonw@gmail.com>
2014-02-20 22:56:12 -0800

    add homepage controller spec

--- dw_realtor/spec/controllers/homepage_controller_spec.rb
+++ dw_realtor/spec/controllers/homepage_controller_spec.rb
@@ -2,4 +2,11 @@ require 'spec_helper'
 
 describe HomepageController do
 
+  describe '#index' do
+    it "is successful" do 
+      get :index
+      expect(response).to be_success
+    end
+  end
+
 end
